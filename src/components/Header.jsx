38e05705d5fb299a3c32b4cd2229f74397f2e77a
Florencia Accastelli <f.accastelli@gmail.com>
2024-05-29 13:57:09 -0300

    configuring head and stylesheet

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+export const Header = () => {
+
+    return (
+        <header className="bg-Very-Pale-Blue h-[235px] rounded-b-[20px] pt-8 px-6">
+            <h1 className="text-Very-Dark-Blue text-2xl font-bold mb-1">Social Media Dashboard</h1>
+            <p className="text-Dark-Grayish-Blue font-bold mb-6">Total Followers: 23,004</p>
+            <hr className="bg-black mb-[19px]"/>
+            <div className="flex justify-between">
+                <p className="text-Dark-Grayish-Blue font-bold"> Dark Mode</p>
+                <input type="checkbox"/>
+            </div>
+        </header>
+    )
+}
