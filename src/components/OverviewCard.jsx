1b7b14ec26e8638e94b1e0e9d5c6366ff5b5c306
Florencia Accastelli <f.accastelli@gmail.com>
2024-05-30 11:06:03 -0300

    dinamic data in cards

--- src/components/OverviewCard.jsx
+++ src/components/OverviewCard.jsx
@@ -1,9 +1,22 @@
-export const OverviewCard = () => {
+import facebookLogo from '../assets/images/icon-facebook.svg';
+import twitterLogo from '../assets/images/icon-twitter.svg';
+import instagramLogo from '../assets/images/icon-instagram.svg';
+import youtubeLogo from '../assets/images/icon-youtube.svg'
+
+const networkLogos = {
+    Facebook: facebookLogo,
+    Twitter: twitterLogo,
+    Instagram: instagramLogo,
+    YouTube: youtubeLogo
+}
+
+export const OverviewCard = ({ user, audience, audienceType, network }) => {
     return (
-        <div>
-            <article>OverviewCard</article>
-        </div>
-        
-        
+        <article className="bg-Light-Grayish-Blue w-[326px] h-[216px] mb-4 rounded-[5px] mx-auto">
+            <img src={networkLogos[network]} alt={`logo ${network}`}/>
+            <p>{user}</p>
+            <p>{audience}</p>
+            <p>{audienceType}</p>
+        </article>
     )
 }
--- src/components/OverviewContainer.jsx
+++ src/components/OverviewContainer.jsx
@@ -1,14 +1,18 @@
 import data from '../../data/data.json';
 import { OverviewCard } from './OverviewCard';
 
-console.log(data.overview)
-
 export const OverviewContainer = () => {
     return (
         <section>
             {
                 data.overview.map(element => {
-                    return <OverviewCard key={element.id} />
+                    return <OverviewCard 
+                    key={element.id}
+                    user= {element.user}
+                    audience={element.audience}
+                    audienceType={element.audienceType}
+                    network={element.network}
+                    />
                 })
             }
         </section>
